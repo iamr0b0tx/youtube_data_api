992880d9076cd384c691059e284ec525d9c73f53
Joshua Christian <joshua3christianx@gmail.com>
2020-04-03 15:50:15 +0100

    update script and build exe

--- ytapi.py
+++ ytapi.py
@@ -3,7 +3,7 @@
 # import the std lib modules
 import os
 from json import dump
-from time import strftime
+from time import strftime, ctime
 from csv import writer
 
 import google.oauth2.credentials
@@ -43,38 +43,53 @@ if __name__ == '__main__':
 	# *DO NOT* leave this option enabled when running in production.
 	os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 	
-	print("NOTE: You can press enter without the dates to use a default day")
-	start_date = input("Type the day you want result to start (YYYY-MM-DD) and press enter: ")
-	end_date = input("Type the day you want result to end (YYYY-MM-DD) and press enter: ")
-	
-	if not start_date.strip():
-		start_date = strftime('%Y-01-01')
-
-	if not end_date.strip():
-		end_date = strftime("%Y-%m-%d")
-
+	# initialize
 	youtubeAnalytics = get_service()
-	data_object = execute_api_request(
-			youtubeAnalytics.reports().query,
-			ids='channel==MINE',
-			startDate=start_date,
-			endDate=end_date,
-			metrics=read_input('metrics.txt'),
-			dimensions=read_input('dimensions.txt'),
-			sort='day'
-	)
 
-	# save as json
-	with open('output.json', 'w') as output_file_object:
-		dump(data_object, output_file_object)
+	# initilize
+	try_again = None
 
-	# save as csv
-	with open('output.csv', 'w', newline='') as output_file_object:
-		wr = writer(output_file_object)
+	while try_again  != 'q':
+		print("\nEdit the [dimensions.txt] and [metrics.txt] to specify the parameters you wish to obtain")
+		print("NOTE: Make sure you have your [client_secret.json] in this folder")
+		print("NOTE: You can press [enter] without the dates to use a default day")
 
-		# get the headers
-		wr.writerow([head["name"] for head in data_object["columnHeaders"]])
+		print("\nResult Time range\n===============")
+		start_date = input("Type the day you want result to start (YYYY-MM-DD) and press enter: ")
+		end_date = input("Type the day you want result to end (YYYY-MM-DD) and press enter: ")
+		
+		if not start_date.strip():
+			start_date = strftime('%Y-01-01')
 
-		# get the rows of data
-		wr.writerows(data_object["rows"])
-	
+		if not end_date.strip():
+			end_date = strftime("%Y-%m-%d")
+
+		data_object = execute_api_request(
+				youtubeAnalytics.reports().query,
+				ids='channel==MINE',
+				startDate=start_date,
+				endDate=end_date,
+				metrics=read_input('metrics.txt'),
+				dimensions=read_input('dimensions.txt'),
+				sort='day'
+		)
+
+		# the output recieved
+		output_filepath = f'results/{ctime().replace(":", "_")}'
+
+		# save as json
+		with open(f'{output_filepath}.json', 'w') as output_file_object:
+			dump(data_object, output_file_object)
+
+		# save as csv
+		with open(f'{output_filepath}.csv', 'w', newline='') as output_file_object:
+			wr = writer(output_file_object)
+
+			# get the headers
+			wr.writerow([head["name"] for head in data_object["columnHeaders"]])
+
+			# get the rows of data
+			wr.writerows(data_object["rows"])
+
+		# if you wish to try again
+		try_again = input("Type ['q' and enter] to quit or just [enter] to continue: ").strip()
